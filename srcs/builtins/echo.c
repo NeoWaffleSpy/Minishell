ae73da6673864e97ac89cab0e700d9309052e044
Nathan CABA <ncaba@e1r8p4.clusters.42paris.fr>
2022-08-09 21:08:42 +0200

    nettoyer la norminette

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -12,24 +12,33 @@
 
 #include "../../include/minishell.h"
 
+static void	doll_search(t_var *var, char **tmp, char **res);
+
 static void	check_single_char_replace(t_var *var, char **tmp, char **res)
 {
 	char	*str;
 
-	if (**tmp == ' ')
-	{
-		*res = ft_buffalloc(*res, '$');
-		return ;
-	}
 	if (**tmp == '0')
+	{
 		cpy_str(res, "minishell");
-	if (**tmp == '?')
+		(*tmp)++;
+	}
+	else if (**tmp == '?')
 	{
 		str = ft_itoa(var->exit_status);
 		cpy_str(res, str);
 		free_garbage(str);
+		(*tmp)++;
 	}
-	(*tmp)++;
+	else if (ft_isdigit(**tmp))
+	{
+		return ;
+		(*tmp)++;
+	}
+	else
+		*res = ft_buffalloc(*res, '$');
+	if (**tmp == '$' )
+		doll_search(var, tmp, res);
 	return ;
 }
 
@@ -41,12 +50,12 @@ static void	doll_search(t_var *var, char **tmp, char **res)
 
 	ttadd = NULL;
 	(*tmp)++;
-	if (ft_isdigit(**tmp) || **tmp == ' ' || **tmp == '?')
+	if (!ft_isalpha(**tmp) || **tmp == '?')
 		return (check_single_char_replace(var, tmp, res));
 	if (**tmp != ' ' && !ft_isdigit(**tmp))
 	{
 		len = 0;
-		while ((*tmp)[len] && (*tmp)[len] != ' ' && (*tmp)[len] != '\"' && (*tmp)[len] != '\'')
+		while ((*tmp)[len] && (ft_isalnum((*tmp)[len]) || (*tmp)[len] == '_'))
 			len++;
 		ttadd = ft_substr(*tmp, 0, len);
 		env = ft_find_env_elem(var->env, ttadd);
